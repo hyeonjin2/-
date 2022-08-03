d043a82a80bb40461dd116b3a084f3c401d54b60
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-03 17:56:02 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2048 ms, Memory: 141068 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		// 배열 초기화
+		int[][] arr = new int[N + 1][N + 1];
+		int[][] sum = new int[N + 1][N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			for (int j = 0; j < N + 1; j++) {
+				arr[i][0] = 0;
+				arr[0][j] = 0;
+			}
+		}
+		for (int i = 1; i < N + 1; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 1; j < N + 1; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
+			}
+		}
+		int x1 = 0;
+		int y1 = 0;
+		int x2 = 0;
+		int y2 = 0;
+		int diffX;
+		// 계산하기
+		for (int i = 0; i < M; i++) {
+			int result = 0;
+			// 인덱스 값 받기
+			st = new StringTokenizer(br.readLine(), " ");
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+
+			result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
+			System.out.println(result);
+		}
+	}
+}
