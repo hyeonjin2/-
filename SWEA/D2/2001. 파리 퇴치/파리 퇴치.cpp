14e662025b7c4e176774c67229f009aecd1dae0f
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-03 21:55:54 +0900

    [D2] Title: 파리 퇴치, Time: 7 ms, Memory: 13,336 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.cpp
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.cpp
@@ -1,96 +1,43 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// float b, c;
-// double d, e, f;
-// char g;
-// char var[256];
-// long long AB;
-// cin >> a;                            // int 변수 1개 입력받는 예제
-// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
-// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
-// cin >> g;                            // char 변수 1개 입력받는 예제
-// cin >> var;                          // 문자열 1개 입력받는 예제
-// cin >> AB;                           // long long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// float b = 1.0, c = 2.0;               
-// double d = 3.0, e = 0.0; f = 1.0;
-// char g = 'b';
-// char var[256] = "ABCDEFG";
-// long long AB = 12345678901234567L;
-// cout << a;                           // int 변수 1개 출력하는 예제
-// cout << b << " " << c;               // float 변수 2개 출력하는 예제
-// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
-// cout << g;                           // char 변수 1개 출력하는 예제
-// cout << var;                         // 문자열 1개 출력하는 예제
-// cout << AB;                          // long long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-#include<iostream>
+public class Solution {
 
-using namespace std;
-
-int main(int argc, char** argv)
-{
-	int test_case;
-	int T;
-	/*
-	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
-	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
-	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
-	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
-	*/
-	//freopen("input.txt", "r", stdin);
-	cin>>T;
-	/*
-	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-	*/
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
-
-		int n, m;
-		cin >> n >> m;
-		int row = n;
-		int col = n;
-		int sum = 0;
-		int result = 0;
-
-		int** matric= new int*[row];
-
-		for (int i = 0; i < row; i++) {
-			matric[i] = new int[col];
-		}
-
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				cin >> matric[i][j];
-			}
-		}
-
-		for (int k = 0; k <= n - m; k++) {
-			for (int i = 0; i < m; i++) {
-				for (int j = 0; j < m; j++) {
-					sum += matric[i + k][j + k];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			// 배열 초기화
+			int[][] arr = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			result = max(result, sum);
-            sum=0;
+			int max = Integer.MIN_VALUE;
+			int sum = 0;
+			// 배열 검색
+			for (int i = 0; i <= N - M; i++) {
+				for (int j = 0; j <= N - M; j++) {
+					sum = 0;
+					// 파리채의 크기만큼 검색 후 max 찾기
+					for (int k = i; k < i + M; k++) {
+						for (int l = j; l < j + M; l++) {
+							sum += arr[k][l];
+						}
+					}
+					max = Math.max(max, sum);
+				}
+			}
+			System.out.println("#" + tc + " " + max);
 		}
-
-		cout << "#" << test_case << " " << result << endl;
-
-		for (int i = 0; i < row; i++) {
-			delete[] matric[i];
-		}
-		delete[] matric;
 
 	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+
 }
