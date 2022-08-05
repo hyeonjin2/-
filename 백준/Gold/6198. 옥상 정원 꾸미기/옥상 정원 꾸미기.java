7d544c9f7e049689fcecd8cf2be56cca02821338
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-05 23:24:36 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 336 ms, Memory: 23564 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Stack<Integer> floors = new Stack<>();
+		long cnt = 0;
+		for (int i = 0; i < N; i++) {
+			int number = Integer.parseInt(br.readLine());
+			if (floors.isEmpty()) {
+				floors.push(number);
+				continue;
+			}
+			if (floors.peek() > number) {
+				cnt += floors.size();
+				floors.push(number);
+			} else {
+				while (!floors.isEmpty()) {
+					if (floors.peek() > number)
+						break;
+					floors.pop();
+				}
+				cnt += floors.size();
+				floors.push(number);
+			}
+		}
+		System.out.println(cnt);
+	}
+}
