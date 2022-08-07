922d27d6588e879a394c34a04d4c8723429f1a92
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-07 14:48:37 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 2212 ms, Memory: 209808 KB -BaekjoonHub

--- 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.java
@@ -12,21 +12,16 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		StringBuilder sb = new StringBuilder();
-		Integer[] numbers = new Integer[N];
-		long[] sum = new long[N];
+		Long[] numbers = new Long[N];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
+			numbers[i] = (long) Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(numbers, (e1, e2) -> {
-			return e1 - e2;
+			return (int) (e1 - e2);
 		});
-		for (int i = 0; i < N; i++) {
-			if (i == 0) {
-				sum[i] = numbers[i];
-				continue;
-			}
-			sum[i] = sum[i - 1] + numbers[i];
+		for (int i = 1; i < N; i++) {
+			numbers[i] += numbers[i - 1];
 		}
 		long result = 0;
 		for (int i = 0; i < K; i++) {
@@ -34,9 +29,9 @@ public class Main {
 			int start = Integer.parseInt(st.nextToken()) - 1;
 			int end = Integer.parseInt(st.nextToken()) - 1;
 			if (start == 0) {
-				result = sum[end];
+				result = numbers[end];
 			} else {
-				result = sum[end] - sum[start - 1];
+				result = numbers[end] - numbers[start - 1];
 			}
 			sb.append(result + "\n");
 		}
