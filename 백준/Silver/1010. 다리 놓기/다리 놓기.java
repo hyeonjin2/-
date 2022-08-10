a373e0a174806bb45bd053c55358f0172649d681
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-10 15:48:02 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 13900 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,29 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int N;
+	static int M;
+	static int[][] count;
 
-	static int[][] calced;
-
-	static int calc(int n, int c) { // nCm = n-1C1+n-1Cm-1
-		// 1. 종료조건
-		if (n == c || c == 0)
-			calced[n][c] = 1;
-		// 2. 반복조건 한 번도 계산하지 않은 경우
-		if (calced[n][c] == 0) {
-			calced[n][c] = calc(n - 1, c) + calc(n - 1, c - 1);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			count = new int[M + 1][N + 1];
+			System.out.println(comb(M, N));
 		}
-		// 3. 계산된 경우
-		return calced[n][c];
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int tc = 1; tc <= T; tc++) {
-			int c = sc.nextInt(); // 작은 거
-			int n = sc.nextInt(); // 큰 거
-			calced = new int[n + 1][c + 1];
-			System.out.println(calc(n, c));
+	// mCn을 구하는 함수
+	private static int comb(int m, int n) {
+		if (m == n || n == 0) {
+			count[m][n] = 1;
+			return count[m][n];
 		}
+		if (count[m][n] == 0) {
+			count[m][n] = comb(m - 1, n) + comb(m - 1, n - 1);
+		}
+		return count[m][n]; // mCn = m-1Cn + m-1Cn-1
 	}
 }
