652cd522619d30e6ad856b499ba92ae8c98e97f7
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-16 13:07:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int weight = sc.nextInt();
+		int count = 0;
+		while (weight > 0) {
+			if (weight % 5 != 0) {
+				if (weight >= 3) {
+					weight -= 3;
+					count++;
+				} else {
+					count = -1;
+					break;
+				}
+			} else {
+				if (weight >= 5) {
+					weight -= 5;
+					count++;
+				} else {
+					count = -1;
+					break;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
