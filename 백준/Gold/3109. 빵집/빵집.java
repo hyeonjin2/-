144c42759a4db38932246fc35d8f745c5b151cd1
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-20 00:47:16 +0900

    [Gold II] Title: 빵집, Time: 368 ms, Memory: 127732 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,50 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, m, Ans;
+	static char[][] map; // 집 정보를 담을 배열
+	static boolean[][] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		// 지도 배열 입력 받기
+		map = new char[n][m];
+		for (int i = 0; i < n; i++) {
+			map[i] = br.readLine().trim().toCharArray();
+		}
+		visited = new boolean[n][m];
+		for (int i = 0; i < n; i++) {
+			if (check(new Point(i, 0)))
+				Ans++;
+		}
+		System.out.println(Ans);
+	}
+
+	// 이동 델타 배열 우상,우,우하
+	static int[] dx = { -1, 0, 1 };
+	static int[] dy = { 1, 1, 1 };
+
+	static boolean check(Point start) {
+		visited[start.x][start.y] = true;
+		if (start.y == m - 1) {
+			return true;
+		}
+		for (int d = 0; d < 3; d++) {
+			Point next = new Point(start.x + dx[d], start.y + dy[d]);
+			if (next.x >= 0 && next.x < n && next.y < m && map[next.x][next.y] == '.') {
+				if (!visited[next.x][next.y] && check(next))
+					return true;
+			}
+		}
+		return false;
+	}
+}
