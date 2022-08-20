1af4300cd22d94264f3a6fa445813291196fd80f
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-20 17:03:43 +0900

    [Gold IV] Title: 알파벳, Time: 1328 ms, Memory: 292268 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -11,16 +11,6 @@ public class Main {
 	static int[][] map;
 	static int n, m, Ans;
 
-	static class Alpabet {
-		Point p;
-		int val;
-
-		public Alpabet(Point p, int val) {
-			this.p = p;
-			this.val = val;
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -34,7 +24,7 @@ public class Main {
 			}
 		}
 		visited = new boolean[26];
-		dfs(new Alpabet(new Point(0, 0), map[0][0]), 0);
+		dfs(new Point(0, 0), 0);
 		System.out.println(Ans);
 	}
 
@@ -42,23 +32,21 @@ public class Main {
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
 
-	private static void dfs(Alpabet cur, int count) {
-		Point p = cur.p;
+	private static void dfs(Point cur, int count) {
 		// 같은 알파벳을 발견했을 경우 count 확인 후 리턴
-		if (visited[map[p.x][p.y]]) {
+		if (visited[map[cur.x][cur.y]]) {
 			Ans = Math.max(Ans, count);
 			return;
 		}
 		// 다른 알파벳인 경우 탐색
-		visited[map[p.x][p.y]] = true;
+		visited[map[cur.x][cur.y]] = true;
 		for (int d = 0; d < 4; d++) {
-			Point np = new Point(p.x + dx[d], p.y + dy[d]);
+			Point np = new Point(cur.x + dx[d], cur.y + dy[d]);
 			if (np.x >= 0 && np.x < n && np.y >= 0 && np.y < m) {
-				Alpabet next = new Alpabet(np, map[np.x][np.y]);
-				dfs(next, count + 1);
+				dfs(np, count + 1);
 			}
 		}
 		// 다른 루트로 DFS 탐색하기 위해 visited 배열 초기화
-		visited[map[p.x][p.y]] = false;
+		visited[map[cur.x][cur.y]] = false;
 	}
 }
