b6fdc4885364d851cf63f160b797607cde7c8223
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-08-25 17:03:50 +0900

    [Gold IV] Title: 네트워크 연결, Time: 488 ms, Memory: 46648 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -2,39 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Node {
-		int vertex, weight;
-		Node next;
+	static class Edge implements Comparable<Edge> {
+		int from, to, weigth;
 
-		public Node(int vertex, int weight, Node next) {
-			this.vertex = vertex;
-			this.weight = weight;
-			this.next = next;
+		public Edge(int from, int to, int weigth) {
+			this.from = from;
+			this.to = to;
+			this.weigth = weigth;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.weigth - o.weigth;
 		}
 	}
 
-	static class Vertex {
-		int no, weight;
-
-		public Vertex(int no, int weight) {
-			this.no = no;
-			this.weight = weight;
-		}
-	}
+	static int V, E;
+	static int[] parents;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int V = Integer.parseInt(br.readLine());
-		int E = Integer.parseInt(br.readLine());
+		V = Integer.parseInt(br.readLine());
+		E = Integer.parseInt(br.readLine());
 
-		Node[] adjList = new Node[V];
 		// 간선 정보 입력 받기.
+		Edge[] edgeList = new Edge[E];
 		StringTokenizer st;
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -42,41 +39,43 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken()) - 1;
 			int weight = Integer.parseInt(st.nextToken());
 
-			adjList[from] = new Node(to, weight, adjList[from]);
-			adjList[to] = new Node(from, weight, adjList[to]);
+			edgeList[i] = new Edge(from, to, weight);
 		}
-		// 프림 알고리즘
-		int[] minEdge = new int[V];
-		boolean[] visited = new boolean[V];
-		// 정점의 가중치 맥스값으로 두기
-		Arrays.fill(minEdge, Integer.MAX_VALUE);
-		// 시작점 가중치 0으로 두기
-		minEdge[0] = 0;
-		int result = 0; // 가중치의 합
-		PriorityQueue<Vertex> pQueue = new PriorityQueue<>((v1, v2) -> v1.weight - v2.weight);
-		pQueue.offer(new Vertex(0, minEdge[0]));
 
+		make();
+		Arrays.sort(edgeList);
 		int cnt = 0;
-		while (!pQueue.isEmpty()) {
-
-			Vertex minVertex = pQueue.poll();
-
-			if (visited[minVertex.no])
-				continue;
-
-			visited[minVertex.no] = true;
-			result += minVertex.weight;
-			if (++cnt == V)
-				break;
-
-			for (Node temp = adjList[minVertex.no]; temp != null; temp = temp.next) {
-				if (!visited[temp.vertex] && minEdge[temp.vertex] > temp.weight) {
-					minEdge[temp.vertex] = temp.weight;
-					pQueue.offer(new Vertex(temp.vertex, minEdge[temp.vertex]));
-				}
-
+		int sum = 0;
+		for (int i = 0; i < E; i++) {
+			Edge temp = edgeList[i];
+			if (union(temp.from, temp.to)) {
+				sum += temp.weigth;
+				if (++cnt == V - 1)
+					break;
 			}
 		}
-		System.out.println(result);
+		System.out.println(sum);
+	}
+
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if (aRoot == bRoot)
+			return false;
+		parents[bRoot] = aRoot;
+		return true;
+	}
+
+	private static int find(int a) {
+		if (parents[a] == a)
+			return a;
+		return parents[a] = find(parents[a]);
+	}
+
+	private static void make() {
+		parents = new int[V];
+		for (int i = 0; i < V; i++) {
+			parents[i] = i;
+		}
 	}
 }
