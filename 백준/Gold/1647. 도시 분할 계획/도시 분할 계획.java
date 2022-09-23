820884dfc2ed9a56cd0e165cb9cb65b44b1f4545
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-09-23 20:31:36 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2368 ms, Memory: 331612 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 도시 분할 계획
+public class Main {
+	static class Edge implements Comparable<Edge> {
+		int from, to, weight;
+
+		public Edge(int from, int to, int weight) {
+			this.from = from;
+			this.to = to;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.weight - o.weight;
+		}
+
+	}
+
+	static int N, M;
+	static Edge[] edgeList;
+	static int[] parents;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		edgeList = new Edge[M];
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+
+			edgeList[i] = new Edge(from, to, weight);
+		}
+
+		make();
+		Arrays.sort(edgeList);
+
+		int sum = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < M; i++) {
+			int from = edgeList[i].from;
+			int to = edgeList[i].to;
+			if (union(from, to)) {
+				sum += edgeList[i].weight;
+				max = Math.max(max, edgeList[i].weight);
+			}
+		}
+		System.out.println(sum - max);
+	}
+
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if (aRoot != bRoot) {
+			parents[bRoot] = aRoot;
+			return true;
+		} else
+			return false;
+	}
+
+	private static int find(int a) {
+		if (parents[a] == a)
+			return a;
+		return parents[a] = find(parents[a]);
+	}
+
+	private static void make() {
+		parents = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parents[i] = i;
+		}
+	}
+
+}
