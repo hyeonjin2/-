e23b6060c4ccd92bf57bed0aa91fabe648d39faf
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-10-19 01:06:26 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 344 ms, Memory: 89304 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -15,7 +15,6 @@ public class Main {
 	static final int INF = Integer.MAX_VALUE;
 	static char[][] map;
 	static int[][] group;
-	static HashMap<Integer, List<Point>> info;
 	static HashSet<Integer> remove;
 	static HashMap<Character, Integer> dir;
 
@@ -36,19 +35,16 @@ public class Main {
 		dir.put('L', 2);
 		dir.put('R', 3);
 
-		info = new HashMap<>();
 		remove = new HashSet<>();
 		group = new int[N][M];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if (group[i][j] == 0) {
-					List<Point> list = new ArrayList<>();
-					info.put(++index, list);
-					setGroup(i, j, index);
+					setGroup(i, j, ++index);
 				}
 			}
 		}
-		System.out.println(info.size() - remove.size());
+		System.out.println(index - remove.size());
 //		print(group);
 	}
 
@@ -56,9 +52,6 @@ public class Main {
 	static int[] dy = { 0, 0, -1, 1 };
 
 	private static void setGroup(int i, int j, int ind) {
-		List<Point> list = info.get(ind);
-		list.add(new Point(i, j));
-		info.put(ind, list);
 		group[i][j] = ind;
 
 		int d = dir.get(map[i][j]);
@@ -69,9 +62,6 @@ public class Main {
 			setGroup(nx, ny, ind);
 		else if (group[nx][ny] != group[i][j]) {
 			// 그룹 2개 합치기
-			List<Point> tlist = info.get(group[nx][ny]);
-			tlist.addAll(list);
-			info.put(group[nx][ny], tlist);
 			remove.add(group[i][j]);
 			group[i][j] = group[nx][ny];
 		}
