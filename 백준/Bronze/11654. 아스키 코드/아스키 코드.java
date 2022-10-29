e5931e7818ceb267af880643be4c555fccfe040c
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2022-10-29 13:13:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12800 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// 아스키 코드
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ans = sc.next().trim().charAt(0);
+		System.out.println(ans);
+	}
+
+}
