0a2ed4edfb2af51618652819d86ff78a1e5ed78c
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2023-07-04 22:23:26 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 84 ms, Memory: 11824 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,82 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M, answer;
+
+	// 사다리, 뱀 정보를 담을 배열 (하나의 칸이 사다리와 뱀을 동시에 가질 수 없으므로 하나의 배열로 관리
+	static List<Point> move;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		move = new ArrayList<>();
+
+		// 사다리 정보 저장
+		for (int i = 0; i < N + M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+
+			Point p = new Point(x, y);
+			move.add(p);
+		}
+
+		bfs();
+
+		System.out.println(answer);
+	}
+
+	static void bfs() {
+		Queue<Point> queue = new ArrayDeque<>();
+		boolean[] visited = new boolean[101];
+		queue.offer(new Point(1, 0));
+		visited[1] = true;
+
+		while (!queue.isEmpty()) {
+			Point cur = queue.poll();
+			int location = cur.x;
+			int count = cur.y;
+			if (location == 100) {
+				answer = count;
+				break;
+			}
+			for (int i = 1; i <= 6; i++) {
+				int next = location + i;
+
+				if (next > 100)
+					continue;
+				if (next == 100) {
+					queue.offer(new Point(next, count + 1));
+					break;
+				}
+				// 사다리 또는 뱀이 있는 칸에 도착했는지 확인
+				for (int m = 0; m < move.size(); m++) {
+					Point p = move.get(m);
+
+					// 사다리 또는 뱀이 있는 칸이라면 이동
+					if (next == p.x) {
+						next = p.y;
+					}
+				}
+				if (!visited[next]) {
+					queue.offer(new Point(next, count + 1));
+					visited[next] = true;
+				}
+			}
+		}
+	}
+
+}
