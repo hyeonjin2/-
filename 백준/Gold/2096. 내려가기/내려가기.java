a656772e7d58330cc40b5415e411b863e7aad0ab
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2023-07-06 12:12:14 +0900

    [Gold V] Title: 내려가기, Time: 320 ms, Memory: 40172 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -11,45 +11,42 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int[] floor = new int[3];
-		int[][] max = new int[3][N];
-		int[][] min = new int[3][N];
+		int[] max = new int[3];
+		int[] min = new int[3];
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		floor[0] = Integer.parseInt(st.nextToken());
-		floor[1] = Integer.parseInt(st.nextToken());
-		floor[2] = Integer.parseInt(st.nextToken());
-
-		max[0][0] = floor[0];
-		max[1][0] = floor[1];
-		max[2][0] = floor[2];
-
-		min[0][0] = floor[0];
-		min[1][0] = floor[1];
-		min[2][0] = floor[2];
-
-		for (int i = 1; i < N; i++) {
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			floor[0] = Integer.parseInt(st.nextToken());
-			floor[1] = Integer.parseInt(st.nextToken());
-			floor[2] = Integer.parseInt(st.nextToken());
+			int f0 = Integer.parseInt(st.nextToken());
+			int f1 = Integer.parseInt(st.nextToken());
+			int f2 = Integer.parseInt(st.nextToken());
 
-			max[0][i] = Math.max(max[0][i - 1] + floor[0], max[1][i - 1] + floor[0]);
-			max[1][i] = Math.max(max[0][i - 1] + floor[1], max[1][i - 1] + floor[1]);
-			max[1][i] = Math.max(max[1][i], max[2][i - 1] + floor[1]);
-			max[2][i] = Math.max(max[1][i - 1] + floor[2], max[2][i - 1] + floor[2]);
+			if (i == 0) {
+				max[0] = f0;
+				max[1] = f1;
+				max[2] = f2;
 
-			min[0][i] = Math.min(min[0][i - 1] + floor[0], min[1][i - 1] + floor[0]);
-			min[1][i] = Math.min(min[0][i - 1] + floor[1], min[1][i - 1] + floor[1]);
-			min[1][i] = Math.min(min[1][i], min[2][i - 1] + floor[1]);
-			min[2][i] = Math.min(min[1][i - 1] + floor[2], min[2][i - 1] + floor[2]);
+				min[0] = f0;
+				min[1] = f1;
+				min[2] = f2;
+
+				continue;
+			}
+			int preMax0 = max[0];
+			int preMax2 = max[2];
+			max[0] = Math.max(max[0], max[1]) + f0;
+			max[2] = Math.max(max[1], max[2]) + f2;
+			max[1] = Math.max(preMax0, Math.max(max[1], preMax2)) + f1;
+
+			int preMin0 = min[0];
+			int preMin2 = min[2];
+			min[0] = Math.min(min[0], min[1]) + f0;
+			min[2] = Math.min(min[1], min[2]) + f2;
+			min[1] = Math.min(preMin0, Math.min(min[1], preMin2)) + f1;
 		}
 
-		int ans1 = Math.max(max[0][N - 1], max[1][N - 1]);
-		ans1 = Math.max(ans1, max[2][N - 1]);
-
-		int ans2 = Math.min(min[0][N - 1], min[1][N - 1]);
-		ans2 = Math.min(ans2, min[2][N - 1]);
+		int ans1 = Math.max(max[0], Math.max(max[1], max[2]));
+		int ans2 = Math.min(min[0], Math.min(min[1], min[2]));
 
 		System.out.println(ans1 + " " + ans2);
 	}
