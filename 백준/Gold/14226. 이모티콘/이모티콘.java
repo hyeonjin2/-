2e2aae5800818995dd1d71c8e3462ad5d843a706
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2023-08-15 18:46:12 +0900

    [Gold IV] Title: 이모티콘, Time: 132 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.java
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 // 이모티콘
 public class Main {
 
-	static class Imoticon {
+	static class Emoticon {
 		int screen;
 		int clip;
 		int time;
 
-		public Imoticon(int screen, int clip, int time) {
+		public Emoticon(int screen, int clip, int time) {
 			this.screen = screen;
 			this.clip = clip;
 			this.time = time;
@@ -26,13 +26,13 @@ public class Main {
 	}
 
 	private static void bfs(int end) {
-		Queue<Imoticon> queue = new ArrayDeque<>();
-		boolean[][] visited = new boolean[2001][2001];
-		queue.offer(new Imoticon(1, 0, 0));
+		Queue<Emoticon> queue = new ArrayDeque<>();
+		boolean[][] visited = new boolean[1001][1001];
+		queue.offer(new Emoticon(1, 0, 0));
 		visited[0][1] = true;
 
 		while (!queue.isEmpty()) {
-			Imoticon cur = queue.poll();
+			Emoticon cur = queue.poll();
 
 			if (cur.screen == end) {
 				System.out.println(cur.time);
@@ -40,17 +40,17 @@ public class Main {
 			}
 
 			// 1. 화면에 있는 이모티콘을 모두 복사해서 클립보드에 저장한다.
-			queue.offer(new Imoticon(cur.screen, cur.screen, cur.time + 1));
+			queue.offer(new Emoticon(cur.screen, cur.screen, cur.time + 1));
 
 			// 2. 클립보드에 있는 모든 이모티콘을 화면에 붙여넣기 한다.
 			if (cur.clip != 0 && cur.screen + cur.clip <= end && !visited[cur.clip][cur.screen + cur.clip]) {
-				queue.offer(new Imoticon(cur.screen + cur.clip, cur.clip, cur.time + 1));
+				queue.offer(new Emoticon(cur.screen + cur.clip, cur.clip, cur.time + 1));
 				visited[cur.clip][cur.screen + cur.clip] = true;
 			}
 
 			// 3. 화면에 있는 이모티콘 중 하나를 삭제한다.
 			if (cur.screen > 0 && !visited[cur.clip][cur.screen - 1]) {
-				queue.offer(new Imoticon(cur.screen - 1, cur.clip, cur.time + 1));
+				queue.offer(new Emoticon(cur.screen - 1, cur.clip, cur.time + 1));
 				visited[cur.clip][cur.screen - 1] = true;
 			}
 		}
