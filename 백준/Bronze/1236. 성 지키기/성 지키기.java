c5a5b76ad5a62beff1f98aa14f666e7c4721eeac
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2023-08-30 14:56:03 +0900

    [Bronze I] Title: 성 지키기, Time: 80 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 성 지키기
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		char[][] map = new char[N][M];
+		int[] row = new int[N];
+		int[] col = new int[M];
+
+		for (int i = 0; i < N; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = input.charAt(j);
+				if (map[i][j] == 'X') {
+					row[i]++;
+					col[j]++;
+				}
+			}
+		}
+
+		int result = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (row[i] == 0 && col[j] == 0) {
+					result++;
+					row[i]++;
+					col[j]++;
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (row[i] == 0) {
+					result++;
+					row[i]++;
+				} else if (col[j] == 0) {
+					result++;
+					col[j]++;
+				}
+			}
+		}
+
+		System.out.println(result);
+
+	}
+}
