40107c9dc30cf8e999980ca98f1d3466bae124d7
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2023-10-09 14:31:32 +0900

    [D3] Title: 힙, Time: 335 ms, Memory: 93,156 KB -BaekjoonHub

--- SWEA/D3/2930. 힙/힙.java
+++ SWEA/D3/2930. 힙/힙.java
@@ -7,13 +7,103 @@ import java.util.StringTokenizer;
 // 힙
 public class Solution {
 
+	static class Heap {
+		int[] arr;
+		int size = 0;
+
+		Heap() {
+			arr = new int[100001];
+		}
+
+		void add(int x) {
+			// 데이터 마지막 인덱스에 삽입
+			arr[++size] = x;
+
+			// 위로 올라가면서 자리 찾기
+			int cur = size;
+			while (cur > 1) {
+				int parent = getParent(cur);
+				// 만약 부모 노드보다 현재 노드가 더 크면 -> swap(위로 올라가기)
+				if (arr[cur] > arr[parent]) {
+					int temp = arr[parent];
+					arr[parent] = arr[cur];
+					arr[cur] = temp;
+
+					// 바뀐 parent부터 다시 자리 찾기
+					cur = parent;
+				} else {
+					break;
+				}
+			}
+		}
+
+		int poll() {
+			int max = arr[1];
+			// root값을 가장 마지막 값으로 바꾼다.
+			arr[1] = arr[size];
+			// 가장 마지막 값을 지운다 -> 0으로 바꾼다.
+			arr[size] = 0;
+			size--;
+			// 바뀐 root값에 따라 heapify를 통해 heap을 정렬한다.
+			heapify();
+
+			return max;
+		}
+
+		// heapify : 모든 non-leaf node인 p에 대해 level order의 역순으로 node p와 p의 child 중
+		// 작은 값(min-heap)을 가진 node가 heap property를 만족할 때 까지 계속해서 swap한다
+		void heapify() {
+			// root에서부터 비교
+			int cur = 1;
+			while (getRight(cur) <= size) {
+				int larger = cur;
+				int left = getLeft(cur);
+				int right = getRight(cur);
+				// 왼쪽 노드와 비교
+				if (arr[left] > arr[larger]) {
+					larger = left;
+				}
+				// 오른쪽 노드와 비교
+				if (arr[right] > arr[larger]) {
+					larger = right;
+				}
+				// 만약 자식 노드가 현재 노드보다 클 경우 -> swap(아래로 내려가기)
+				if (larger != cur) {
+					int temp = arr[cur];
+					arr[cur] = arr[larger];
+					arr[larger] = temp;
+					// larger부터 다시 heapify 진행
+					cur = larger;
+				} else {
+					break;
+				}
+			}
+		}
+
+		boolean isEmpty() {
+			return size == 0;
+		}
+
+		int getParent(int child) {
+			return child >> 1;
+		}
+
+		int getLeft(int parent) {
+			return parent << 1;
+		}
+
+		int getRight(int parent) {
+			return parent << 1 | 1;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine()); // 연산 횟수
-			PriorityQueue<Integer> heap = new PriorityQueue<>((e1, e2) -> e2 - e1);
+			Heap heap = new Heap();
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,7 +112,7 @@ public class Solution {
 				// 삽입
 				case 1:
 					int num = Integer.parseInt(st.nextToken());
-					heap.offer(num);
+					heap.add(num);
 					break;
 				// 삭제
 				case 2:
