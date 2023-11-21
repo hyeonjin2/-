2887d25e20a53b50485bd1d5a4b37816717345be
hyeonjin2 <91713287+hyeonjin2@users.noreply.github.com>
2023-11-21 18:05:31 +0900

    [Bronze I] Title: 성 지키기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.cc
+++ 백준/Bronze/1236. 성 지키기/성 지키기.cc
@@ -1,65 +1,57 @@
 #include <iostream>
-
 using namespace std;
-
-int n, m;
-int *row, *col;
 
 int main() {
 
-	cin >> n;
-	cin >> m;
+	int n, m;
+	cin >> n >> m;
 
-	row = new int[n];
+	char** map = new char* [n];
 	for (int i = 0; i < n; i++) {
-		row[i] = 0;
+		map[i] = new char[m];
 	}
-	col = new int[m];
-	for (int i = 0; i < m; i++) {
-		col[i] = 0;
-	}
-
 
 	for (int i = 0; i < n; i++) {
 		string str;
 		cin >> str;
 		for (int j = 0; j < m; j++) {
-			if (str[j] == 'X') {
-				row[i]++;
-				col[j]++;
-			}
+			map[i][j] = str[j];
 		}
 	}
 
-	// 모든 행과 모든 열에 한 명 이상의 경비원
-	// 몇 명의 경비원을 최소로 추가해야 조건을 만족하는가
-
-	int cnt = 0;
-
+	// row 검사
+	int row_cnt = n;
 	for (int i = 0; i < n; i++) {
+		bool flag = false;
 		for (int j = 0; j < m; j++) {
-			if (row[i] == 0 && col[j] == 0) {
-				cnt++;
-				row[i]++;
-				col[j]++;
-			}
+			if (map[i][j] == 'X')
+				flag = true;
 		}
+		if (flag)
+			row_cnt--;
 	}
+
+	// col 검사
+	int col_cnt = m;
+	for (int j = 0; j < m; j++) {
+		bool flag = false;
+		for (int i = 0; i < n; i++) {
+			if (map[i][j] == 'X')
+				flag = true;
+		}
+		if (flag)
+			col_cnt--;
+	}
+
+	
+	cout << max(row_cnt, col_cnt);
+
 
 	for (int i = 0; i < n; i++) {
-		if (row[i] == 0) {
-			row[i]++;
-			cnt++;
-		}
-	}
-	for (int j = 0; j < m; j++) {
-		if (col[j] == 0) {
-			col[j]++;
-			cnt++;
-		}
+		delete map[i];
 	}
 
-	cout << cnt;
+	delete map;
 
 	return 0;
 }
